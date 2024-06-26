550fb80cf830d5f52e332fa255def2872d830c14
atif11jutt <atif11jutt@gmail.com>
2024-06-26 21:12:33 +0500

    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 // custom JS
+
+$(document).ready(function(){
+    function isMobile() {
+        return window.matchMedia("only screen and (max-width: 760px)").matches;
+    }
+
+    $('.nav-link').on('click', function() {
+        if (isMobile()) {
+            var targetOffset = $('#tab-header').offset().top;
+            $('html, body').animate({ scrollTop: targetOffset - 10 }, 'fast');
+        }
+    });
+});
